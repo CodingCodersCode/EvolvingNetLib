95f5e656e23b09a5186f9fe1bfae9ee34e9a62e7
ghc <guhaicheng@ccbtrust.com.cn>
2018-07-26 15:19:27 +0800

    1.可忽略。

--- app/src/main/java/com/demo/evolving/net/lib/CCApplication.java
+++ app/src/main/java/com/demo/evolving/net/lib/CCApplication.java
@@ -28,13 +28,6 @@ public class CCApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        refWatcher = LeakCanary.install(this);
-        // Normal app init code...
 
         initRxNetManager();
     }
