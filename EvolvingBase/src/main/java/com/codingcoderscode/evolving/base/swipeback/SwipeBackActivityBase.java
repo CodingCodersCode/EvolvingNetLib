9404bb99e278f61d044ad8a8ab2efe3c3c1576cc
ghc <guhaicheng@ccbtrust.com.cn>
2018-08-14 15:44:01 +0800

    1.以拷贝(https://github.com/ikew0ng/SwipeBackLayout)源码方式添加滑动关闭功能相关代码。至于滑动关闭功能代码，是否会与RxLifeCycle相关功能代码产生冲突，暂未做相关测试。

---
+++ EvolvingBase/src/main/java/com/codingcoderscode/evolving/base/swipeback/SwipeBackActivityBase.java
@@ -0,0 +1,20 @@
+package com.codingcoderscode.evolving.base.swipeback;
+
+/**
+ * Date：2018/8/14 15:28
+ * <p>
+ * author: ghc
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+}
---
+++ EvolvingBase/src/main/java/com/codingcoderscode/evolving/base/swipeback/SwipeBackActivityHelper.java
@@ -0,0 +1,47 @@
+package com.codingcoderscode.evolving.base.swipeback;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.codingcoderscode.evolving.base.R;
+
+/**
+ * Date：2018/8/14 15:29
+ * <p>
+ * author: ghc
+ */
+public class SwipeBackActivityHelper {
+    private Activity mActivity;
+
+    private SwipeBackLayout mSwipeBackLayout;
+
+    public SwipeBackActivityHelper(Activity activity) {
+        mActivity = activity;
+    }
+
+    @SuppressWarnings("deprecation")
+    public void onActivityCreate() {
+        mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mActivity.getWindow().getDecorView().setBackgroundDrawable(null);
+        mSwipeBackLayout = (SwipeBackLayout) LayoutInflater.from(mActivity).inflate(
+                R.layout.swipeback_layout, null);
+    }
+
+    public void onPostCreate() {
+        mSwipeBackLayout.attachToActivity(mActivity);
+    }
+
+    public View findViewById(int id) {
+        if (mSwipeBackLayout != null) {
+            return mSwipeBackLayout.findViewById(id);
+        }
+        return null;
+    }
+
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mSwipeBackLayout;
+    }
+}
